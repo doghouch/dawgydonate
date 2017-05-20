d66189a3e4e994af7b7c3e94b1eb9b8e26a8216a
doghouch <aaronryan2001@gmail.com>
2017-05-19 21:48:49 -0400

    Re-added ProcessOut

--- donate.php
+++ donate.php
@@ -28,48 +28,92 @@ function get_between($string, $start, $end)
     return substr($string, $ini, $len);
 }
 
-if (in_array($amt , array('5', '10', '20',  '40'))) {
-    
-    $url  = 'https://www.paypal.com/cgi-bin/webscr';
-    $post = array(
-        'business' => $paypaladdress,
-        'cmd' => '_xclick',
-        'item_name' => 'Donation - $' . $amt . '',
-        'item_number' => '0',
-        'page_style' => $paypalstyle,
-        'amount' => $amt,
-        'currency_code' => 'USD',
-        'cancel_return' => $cancel_url,
-        'return' => $success_url,
-        'submit' => '1'
+
+if (in_array($amt, array(
+    '5',
+    '10',
+    '20',
+    '40'
+))) {
+    if ($_POST['processor'] === "paypal") {
+        $post = array(
+            'business' => $paypaladdress,
+            'cmd' => '_xclick',
+            'item_name' => 'Donation - $' . $amt . '',
+            'item_number' => '0',
+            'page_style' => $paypalstyle,
+            'amount' => $amt,
+            'currency_code' => 'USD',
+            'cancel_return' => $cancel_url,
+            'return' => $success_url,
+            'submit' => '1'
+            
+        );
         
-    );
-    
-    
-    
-    
-    $ch = curl_init();
-    
-    
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_VERBOSE, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 1);
-    
-    
-    $result = curl_exec($ch);
-    
-    $url = get_between($result, 'Location: ', 'Cache-Control');
-    $url = str_replace("Content-Encoding: gzip", "", $url);
-    
-    curl_close($ch);
-    
+        
+        
+        
+        $ch = curl_init();
+        
+        
+        curl_setopt($ch, CURLOPT_URL, 'https://www.paypal.com/cgi-bin/webscr');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        
+        
+        $result = curl_exec($ch);
+        
+        $url = get_between($result, 'Location: ', 'Cache-Control');
+        $url = str_replace("Content-Encoding: gzip", "", $url);
+        
+        curl_close($ch);
+        
 ?>
+   <script>var url = '<?php
+        print($url);
+?>';</script>
     <meta http-equiv="refresh" content="0; url=<?php
-    print($url);
+        print($url);
 ?>">
 <?php
+    }
+    
+    if ($_POST['processor'] === "other") {
+        
+        $url = 'https://api.processout.com/invoices';
+        
+        
+        
+        
+        $ch = curl_init();
+        
+        
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_USERPWD, $accessproject . ":" . $accesskey);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "currency=USD&name=Donation&amount=" . $amt . "&statement_descriptor=US" . $amt . " at dawgyTIPS&return_url=" . $success_url . "&cancel_url=" . $cancel_url . "");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        
+        $result = curl_exec($ch);
+        
+        $url = get_between($result, '"url":"', '","created_at"');
+        
+        curl_close($ch);
+        
+?>
+   <script>var url = '<?php
+        print($url);
+?>';</script>
+    <meta http-equiv="refresh" content="0; url=<?php
+        print($url);
+?>">
+<?php
+    }
 }
 
 ?>
